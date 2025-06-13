2a46977c9327f16cf39195e44f27c36f3bf0fbeb
Arbaan <mohammed.arbaan2@gmail.com>
2025-06-14 01:14:10 +0530

    fix url for api

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import requests
+
+res = requests.get("https://zenquotes.io/api/random")
+
+if res.status_code == 200:
+    data = res.json()
+else:
+    print("There was an error, code:", res.status_code)
 
