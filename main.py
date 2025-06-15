a391ed68da6b6a9352b0aa62f83eb01da2677f72
Arbaan <mohammed.arbaan2@gmail.com>
2025-06-15 14:07:09 +0530

    make output prettier using rich lib

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import requests
+from rich.console import Console
 
 res = requests.get("https://zenquotes.io/api/random")
+console = Console()
 
 if res.status_code == 200:
     data = res.json()
 else:
     print("There was an error, code:", res.status_code)
 
+console.print(f":sparkles:[purple]{data[0]['q']}[/purple]:sparkles:", " - ", end = " ")
+console.print(f"[green]{data[0]['a']}[/green]")
